fdc85f775b864c82eaa68189b6051a3fa12544f8
gem-saksham <saksham.khanna@geminisolutions.com>
2024-11-05 17:11:59 +0530

    add cors for config

--- src/main/java/CustomerDatabaseManagement/config/WebConfig.java
+++ src/main/java/CustomerDatabaseManagement/config/WebConfig.java
@@ -9,8 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow CORS for all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow your frontend's origin (React)
+        registry.addMapping("/**") // Allow CORS for all endpoints.allowedOrigins("http://localhost:3000", "https://customer-managment-ui-2.onrender.com/") // Allow your frontend's origin (React)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // Allow specific HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (cookies, authentication)
